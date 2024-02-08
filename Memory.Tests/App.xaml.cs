fbf4a5076a972976689cc48b352afdc20e93d61b
szaaamerik <126014478+szaaamerik@users.noreply.github.com>
2024-02-08 02:56:26 -0800

    test commit

---
+++ Memory.Tests/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Memory.Tests;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void App_OnExit(object sender, ExitEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Memory.Tests/Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Memory.Tests.Views;
+
+public partial class MainWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
